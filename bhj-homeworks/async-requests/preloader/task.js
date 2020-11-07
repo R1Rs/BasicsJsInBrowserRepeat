e9a1ec6c44326b0fb9b83890eb18d936e47498ef
Renat <suleymanov_renat@mail.ru>
2020-11-07 22:59:55 +0300

    Задание 1

--- bhj-homeworks/async-requests/preloader/task.js
+++ bhj-homeworks/async-requests/preloader/task.js
@@ -1,8 +1,25 @@
 let xhr = new XMLHttpRequest();
 xhr.open("GET", "https://netology-slow-rest.herokuapp.com/");
-xhr.setRequestHeader("content-type","application/json");
 xhr.send();
 xhr.responseType = "json";
-let items = document.querySelector(".item");
-console.log(items)
-xhr.onreadystatechange = ()=>{items.innerHTML += xhr.response;}
+xhr.onreadystatechange = ()=>{
+
+if (xhr.status == 200 & xhr.readyState == 4) {
+    document.querySelector(".loader_active").classList.remove("loader_active");
+
+    let item = document.querySelector(".item");
+    let valute = xhr.response.response.Valute;
+
+    for (prop in valute) {
+        let itemCode = valute[prop].CharCode;
+        let itemValue = valute[prop].Value;
+        let itemCurrency = valute[prop].Name;
+
+        item.insertAdjacentHTML("beforebegin", `<div class="item">
+                                                <div class=item__code>${itemCode}</div>
+                                                <div class="item__value">${itemValue}</div>
+                                                <div class="item__currensy">${itemCurrency}</div>
+                                                </div>`)
+    }
+}
+}
