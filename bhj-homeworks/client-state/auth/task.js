4b76cabca851555ba54ffaf5710e7d0af1b809fd
Renat <suleymanov_renat@mail.ru>
2020-11-15 15:16:47 +0300

    Задание 3

--- bhj-homeworks/client-state/auth/task.js
+++ bhj-homeworks/client-state/auth/task.js
@@ -1,23 +1,57 @@
-window.addEventListener("load", () => {
-    document.querySelector(".signin").classList.add("signin_active");
-});
+window.addEventListener("load", init);
 
 let button = document.querySelector(".btn");
+let signIn = document.querySelector(".signin");
+let welcome = document.querySelector(".welcome");
+let userSpan = document.querySelector("#user_id");
+
 button.addEventListener("click", sendForm);
 
 function sendForm(elem) {
-    // elem.preventDefault();
+    elem.preventDefault();
     let formSignin = document.querySelector("#signin__form");
     let formForSend = new FormData(formSignin);
 
     let xhr = new XMLHttpRequest();
     xhr.open("POST", "https://netology-slow-rest.herokuapp.com/auth.php");
-    xhr.send(formForSend);
-
+  
     xhr.addEventListener("readystatechange", () => {
-        if (xhr.readyState == 4 & xhr.status == 200) {
-            console.log(xhr.response)
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            answerServer(xhr.response);
+        } else if (xhr.status !=200) {
+            alert("Не удалось связаться с сервером");
         }
     })
+    xhr.send(formForSend);
     formSignin.reset();
+}
+
+function answerServer(answer) {
+    let answerStatus = JSON.parse(answer).success;
+        if (answerStatus) {
+        let user = JSON.parse(answer).user_id;
+        userSpan.textContent = user;
+        document.querySelector(".signin").classList.remove("signin_active");
+        welcome.classList.add("welcome_active");
+        localStorage.user = user;
+    } else {
+        alert ("Неверные логин/пароль");
+    }
+}
+
+function memoryUser(user) {
+    if (localStorage.user) {
+        welcome.classList.add("welcome_active");
+        userSpan.textContent = localStorage.user;
+    } else {
+        showForm();
+    }
+}
+
+function showForm() {
+    signIn.classList.add("signin_active");
+}
+
+function init() {
+    memoryUser();
 }
